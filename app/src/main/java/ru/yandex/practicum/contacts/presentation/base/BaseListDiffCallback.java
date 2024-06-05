36dc1666ecc66d331714fc29aafe8f6d4c96e036
Панченко Захар <zakhar-panchenko@yandex.ru>
2024-06-05 08:26:27 +0300

    Внес правки согласно рекомендациям.

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -3,21 +3,14 @@ package ru.yandex.practicum.contacts.presentation.base;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
 
-public class BaseListDiffCallback<BaseListDiffCallback> extends DiffUtil.ItemCallback<BaseListDiffCallback>  implements ListDiffInterface<BaseListDiffCallback>{
+public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUtil.ItemCallback<T>{
 
       @Override
-    public boolean areItemsTheSame(@NonNull BaseListDiffCallback oldItem, @NonNull BaseListDiffCallback newItem) {
-       //return false;
-       return theSameAs((BaseListDiffCallback) newItem);
-    }
-
-    @Override
-    public boolean areContentsTheSame(@NonNull BaseListDiffCallback oldItem, @NonNull BaseListDiffCallback newItem) {
-        return false;
-    }
-
-    @Override
-    public boolean theSameAs(BaseListDiffCallback baseListDiffCallback) {
-        return false;
-    }
+      public boolean areItemsTheSame(@NonNull T oldItem, @NonNull T newItem) {
+        return oldItem.theSameAs(newItem);
+      }
+      @Override
+      public boolean areContentsTheSame(@NonNull T oldItem, @NonNull T newItem) {
+          return oldItem.equals(newItem);
+      }
 }
